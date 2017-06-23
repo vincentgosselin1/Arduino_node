a72e1e1c11f421ead7a9ba64b1d1a769a65871ee
vincentgosselin1 <vincentgosselin1@gmail.com>
2017-06-23 14:40:21 -0400

    alottt better

--- with_nano/Arduino_LoRa_Ping_Pong_v2/Arduino_LoRa_Ping_Pong_v2.cpp
+++ with_nano/Arduino_LoRa_Ping_Pong_v2/Arduino_LoRa_Ping_Pong_v2.cpp
@@ -3,129 +3,49 @@
 
 //This is using a nice library to simplify the code.
 #include "Lora_simplified.h"
+//Custom tools for a better code.
+#include "arduinotools.h"
+
+//Testing the sending of values  
+int i = 0;
+
+//SmarWait timer, from arduinotools lib.
+SmartWait timer1;
 
 void setup()
 {
-  int e;
-  
-  // Open serial communications and wait for port to open:
-#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0 
-  SerialUSB.begin(38400);
-#else
-  Serial.begin(38400);  
-#endif 
-
-  // Print a start message
-  PRINT_CSTSTR("%s","Simple LoRa ping-pong with the gateway\n");  
-
-#ifdef ARDUINO_AVR_PRO
-  PRINT_CSTSTR("%s","Arduino Pro Mini detected\n");
-#endif
-
-#ifdef ARDUINO_AVR_NANO
-  PRINT_CSTSTR("%s","Arduino Nano detected\n");
-#endif
-
-#ifdef ARDUINO_AVR_MINI
-  PRINT_CSTSTR("%s","Arduino MINI/Nexus detected\n");
-#endif
-
-#ifdef __MK20DX256__
-  PRINT_CSTSTR("%s","Teensy31/32 detected\n");
-#endif
-
-#ifdef __SAMD21G18A__ 
-  PRINT_CSTSTR("%s","Arduino M0/Zero detected\n");
-#endif
-
-  // Power ON the module
-  sx1272.ON();
-  
-  // Set transmission mode and print the result
-  e = sx1272.setMode(loraMode);
-  PRINT_CSTSTR("%s","Setting Mode: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-
-  // enable carrier sense
-  sx1272._enableCarrierSense=true;
-    
-  // Select frequency channel
-  e = sx1272.setChannel(DEFAULT_CHANNEL);
-  PRINT_CSTSTR("%s","Setting Channel: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-  
-  // Select amplifier line; PABOOST or RFO
-#ifdef PABOOST
-  sx1272._needPABOOST=true;
-  // previous way for setting output power
-  // powerLevel='x';
-#else
-  // previous way for setting output power
-  // powerLevel='M';  
-#endif
-
-  // previous way for setting output power
-  // e = sx1272.setPower(powerLevel); 
-
-  e = sx1272.setPowerDBM((uint8_t)MAX_DBM); 
-  PRINT_CSTSTR("%s","Setting Power: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-  
-  // Set the node address and print the result
-  e = sx1272.setNodeAddress(node_addr);
-  PRINT_CSTSTR("%s","Setting node addr: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-  
-  // Print a success message
-  PRINT_CSTSTR("%s","SX1272 successfully configured\n");
-
-  delay(500);
+  Lora_init();
 }
 
 
 void loop(void)
 {
-  uint8_t r_size;
-  int e;
+  //Constructing a string with a new value of i everytime.
+  i++;
 
-  sx1272.CarrierSense();
-
-  sx1272.setPacketType(PKT_TYPE_DATA);
-
-  //r_size=sprintf((char*)message, "This is a test");
-  //Testing the sending of values  
-  int i = 0;
-
-  while (1) {
-
-      //Constructing a string with a new value of i everytime.
-      i++;
-      r_size=sprintf((char*)message, "\\!Value of i is : %d", i);
-
-      PRINT_CSTSTR("%s","Sending Ping");  
-      PRINTLN;
-            
-      e = sx1272.sendPacketTimeoutACK(DEFAULT_DEST_ADDR, message, r_size);
-
-      // this is the no-ack version
-      // e = sx1272.sendPacketTimeout(DEFAULT_DEST_ADDR, message, r_size);
-            
-      PRINT_CSTSTR("%s","Packet sent, state ");
-      PRINT_VALUE("%d", e);
-      PRINTLN;
-      
-      if (e==3)
-          PRINT_CSTSTR("%s","No Pong!");
-        
-      if (e==0)
-          PRINT_CSTSTR("%s","Pong received from gateway!");      
-
-      PRINTLN;
-      
-      delay(10000);    
+  //wait 5 seconds
+  if(timer1.wait(5000)) 
+  {
+    //string to send
+    char string[100];
+    sprintf(string, "\\!Value of i is : %d", i);
+    Lora_send_string(string);
   }          
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- with_nano/Arduino_LoRa_Ping_Pong_v2/libraries/Lora_simplified/Lora_simplified.h
+++ with_nano/Arduino_LoRa_Ping_Pong_v2/libraries/Lora_simplified/Lora_simplified.h
@@ -36,8 +36,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // please uncomment only 1 choice
 //
-//#define ETSI_EUROPE_REGULATION
-#define FCC_US_REGULATION
+//#define ETSI_EUROPE_REGULATION //A Utiliser en France,(Europe)
+#define FCC_US_REGULATION //A Utiliser au Canada,US, (America)
 //#define SENEGAL_REGULATION
 /////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
@@ -52,8 +52,8 @@
 // IMPORTANT
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // please uncomment only 1 choice
-//#define BAND868
-#define BAND900
+//#define BAND868 //A Utiliser en France,(Europe)
+#define BAND900 //A Utiliser au Canada,US, (America)
 //#define BAND433
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -100,9 +100,135 @@ const uint32_t DEFAULT_CHANNEL=CH_00_433;
 
 #define DEFAULT_DEST_ADDR 1
 
-uint8_t message[100];
+//Remove by Vincent Gosselin, 
+//uint8_t message[100];
 
 int loraMode=LORAMODE;
+//Added by Vincent Gosselin
+uint8_t r_size;
+int e;
+
+void Lora_init(void){
+	
+	//int e;  
+	  // Open serial communications and wait for port to open:
+	#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0 
+	  SerialUSB.begin(38400);
+	#else
+	  Serial.begin(38400);  
+	#endif 
+
+	  // Print a start message
+	  PRINT_CSTSTR("%s","Simple LoRa ping-pong with the gateway\n");  
+
+	#ifdef ARDUINO_AVR_PRO
+	  PRINT_CSTSTR("%s","Arduino Pro Mini detected\n");
+	#endif
+
+	#ifdef ARDUINO_AVR_NANO
+	  PRINT_CSTSTR("%s","Arduino Nano detected\n");
+	#endif
+
+	#ifdef ARDUINO_AVR_MINI
+	  PRINT_CSTSTR("%s","Arduino MINI/Nexus detected\n");
+	#endif
+
+	#ifdef __MK20DX256__
+	  PRINT_CSTSTR("%s","Teensy31/32 detected\n");
+	#endif
+
+	#ifdef __SAMD21G18A__ 
+	  PRINT_CSTSTR("%s","Arduino M0/Zero detected\n");
+	#endif
+
+	  // Power ON the module
+	  sx1272.ON();
+	  
+	  // Set transmission mode and print the result
+	  e = sx1272.setMode(loraMode);
+	  PRINT_CSTSTR("%s","Setting Mode: state ");
+	  PRINT_VALUE("%d", e);
+	  PRINTLN;
+
+	  // enable carrier sense
+	  sx1272._enableCarrierSense=true;
+	    
+	  // Select frequency channel
+	  e = sx1272.setChannel(DEFAULT_CHANNEL);
+	  PRINT_CSTSTR("%s","Setting Channel: state ");
+	  PRINT_VALUE("%d", e);
+	  PRINTLN;
+	  
+	  // Select amplifier line; PABOOST or RFO
+	#ifdef PABOOST
+	  sx1272._needPABOOST=true;
+	  // previous way for setting output power
+	  // powerLevel='x';
+	#else
+	  // previous way for setting output power
+	  // powerLevel='M';  
+	#endif
+
+	  // previous way for setting output power
+	  // e = sx1272.setPower(powerLevel); 
+
+	  e = sx1272.setPowerDBM((uint8_t)MAX_DBM); 
+	  PRINT_CSTSTR("%s","Setting Power: state ");
+	  PRINT_VALUE("%d", e);
+	  PRINTLN;
+	  
+	  // Set the node address and print the result
+	  e = sx1272.setNodeAddress(node_addr);
+	  PRINT_CSTSTR("%s","Setting node addr: state ");
+	  PRINT_VALUE("%d", e);
+	  PRINTLN;
+	  
+	  // Print a success message
+	  PRINT_CSTSTR("%s","SX1272 successfully configured\n");
+
+	  delay(500);
+
+	/*The following was included in the loop, but happened only once in original code...*/
+	//uint8_t r_size;
+	//int e;
+
+	sx1272.CarrierSense();
+
+	sx1272.setPacketType(PKT_TYPE_DATA);
+	/************************************/
+}
+
+void Lora_send_string(char * string){
+
+	//Message to send. 100 characters maximum.
+	uint8_t message[100];
+
+	//r_size=sprintf((char*)message, "\\!Value of i is : %d", i);
+	r_size=sprintf((char*)message, string);
+
+	PRINT_CSTSTR("%s","Sending Ping");  
+	PRINTLN;
+	    
+	e = sx1272.sendPacketTimeoutACK(DEFAULT_DEST_ADDR, message, r_size);
+
+	// this is the no-ack version
+	// e = sx1272.sendPacketTimeout(DEFAULT_DEST_ADDR, message, r_size);
+	    
+	PRINT_CSTSTR("%s","Packet sent, state ");
+	PRINT_VALUE("%d", e);
+	PRINTLN;
+
+	if (e==3)
+	  PRINT_CSTSTR("%s","No Pong!");
+
+	if (e==0)
+	  PRINT_CSTSTR("%s","Pong received from gateway!");      
+
+	PRINTLN;
+
+	//delay(10000);
+}
+
 
 
 #endif

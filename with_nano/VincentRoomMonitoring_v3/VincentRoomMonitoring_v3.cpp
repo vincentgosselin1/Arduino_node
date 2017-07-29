afcd9e3519d8dbba8eea38ce5eff7170560477d7
vincentgosselin1 <vincentgosselin1@gmail.com>
2017-07-29 19:04:08 -0400

    critical sensors every 10min, non-critical every hour. Need to seed to confirm accuracy overtime

--- with_nano/VincentRoomMonitoring_v3/VincentRoomMonitoring_v3.cpp
+++ with_nano/VincentRoomMonitoring_v3/VincentRoomMonitoring_v3.cpp
@@ -5,10 +5,11 @@ The following is a program to monitor
 all the sensors execpt CO, LUX sensor in Vincent's Room.
 
 The state machine of the program :
-1 : Deep sleep (0.27mA) for 30sec
-2 : scan all the sensors
-3 : Construct a string to send to InfluxDB (inside a AWS server)
-4 : Send the string using the Lora Module
+1 : Deep sleep (0.27mA) for 1h and 10minutes
+2 : if 10 minutes done, SCAN soil/air sensors + battery,
+3 : if 1h done, SCAN O3/Dust/GPS
+4 : Construct a string to send to InfluxDB (inside a AWS server)
+5 : Send the string using the Lora Module
 */
 
 //This is using a nice library to simplify the code.
@@ -40,15 +41,17 @@ The state machine of the program :
 //#define TEN_MINUTES_TIMEOUT 490
 //#define TWO_MINUTES_TIMEOUT 98
 //#define TWO_MINUTES_TIMEOUT 300
-#define TWO_MINUTES_TIMEOUT 120 // in seconds
-#define FIVE_MINUTES_TIMEOUT 300 // IN SECONDES
 #define ONE_MINUTE_TIMEOUT 60
+#define TWO_MINUTES_TIMEOUT 120 // 
+#define FIVE_MINUTES_TIMEOUT 300 // IN SECONDES
+#define TEN_MINUTES_TIMEOUT 600 
+#define ONE_HOUR_TIMEOUT 3600
 //watchdog_counter = 49 for 0.85min.
 //watchdog_counter = 98 for 1.7min on Grafana
 //watchdog_counter = 490 for 8.43min on Grafana.
 //watchdog_counter = 3350 for 58min? Actually equals to 57min30sec. Close Enough!
 //volatile unsigned int watchdog_counter = 3350;
-volatile unsigned int long_watchdog_counter = FIVE_MINUTES_TIMEOUT;
+volatile unsigned int long_watchdog_counter = ONE_HOUR_TIMEOUT;
 volatile unsigned int short_watchdog_counter = 0;
 
 ISR(WDT_vect) {
@@ -108,7 +111,7 @@ void setup()
 void loop(void)
 {
   //Scanning of O3, Dust, GPS.
-  if((long_watchdog_counter > FIVE_MINUTES_TIMEOUT)){
+  if((long_watchdog_counter > ONE_HOUR_TIMEOUT)){
 
     sleep_disable(); 
     ADCSRA |= (1<<ADEN); //Enable ADC
@@ -134,7 +137,7 @@ void loop(void)
   }
 
   //Scanning Soil humidity, soil temperature, air humidity, air temperature, battery voltage.
-  if((short_watchdog_counter > TWO_MINUTES_TIMEOUT)){
+  if((short_watchdog_counter > TEN_MINUTES_TIMEOUT)){
 
     sleep_disable(); 
     ADCSRA |= (1<<ADEN); //Enable ADC
@@ -319,166 +322,3 @@ void short_sensor_node_execute(void){
   Serial.end();
 
 }
-
-// void sensor_node_execute(void){
-
-//   //Serial.println(" ");
-//   //delay(1000);
-//   //Serial.print("Wake up now");//Serial.println(millis(),6);
-//   delay(1000);
-//   //Serial.println("Hello world!");
-
-//   //Lora init
-//   Lora_init();
-//   delay(1000);
-
-// /* Sensor scanning begins */
-
-//   //CO sensor is to start first because of long acquiring time.
-//   // int state = MQ7_Sensor.get_preheat_state();
-//   // delay(1000);
-
-//   //O3 sensor.
-//   O3_spec_sensor.scan();
-//   delay(1000);
-
-//   //Dust sensor
-//   sharp_dust_sensor.scan();
-//   delay(1000);
-
-//   //GPS
-//   gps_sensor.scan();
-//   delay(1000);
-
-//   // //Luminosity sensor
-//   //lux_sensor.scan();
-//   //delay(1000);
-
-//   //Soil moisture
-//   soil_moisture.scan();
-//   delay(1000);
-
-//   //Soil temperature
-//   DFR0198.scan();
-//   delay(1000);
-
-//   //Scan temperature and humidity
-//   dht11.scan();
-//   delay(1000);
-
-//   // //Finish CO sensor
-//   // while (state != 3){
-//   //    Serial.println(state);   
-//   //   delay(1000);
-//   //   state = MQ7_Sensor.get_preheat_state();
-//   // }
-//   // MQ7_Sensor.scan();
-
-// /* Sensor done scanning */
-
-// /* Preparing LoRa transmission */  
-
-//   // float O3_concentration = O3_spec_sensor.get_concentration(); //in ppb.
-//   // //int CO_concentration = MQ7_Sensor.get_CO_concentration(); //in ppm. TO BE MODIFIED.
-//   // float dust_concentration = sharp_dust_sensor.get_concentration(); //in micro gram per cubic meter.
-//   // float lat = gps_sensor.get_lat();
-//   // float lon = gps_sensor.get_lon();
-//   // unsigned int lux = lux_sensor.get_lux();
-//   // int soil_humidity = soil_moisture.get_moisture();
-//   // float soil_temperature = DFR0198.get_temperature();
-//   // int air_humidity = dht11.get_humidity();
-//   // int air_temperature = dht11.get_temperature();
-
-//   int O3_concentration = (int)O3_spec_sensor.get_concentration(); //in ppb.
-//   //int CO_concentration = MQ7_Sensor.get_CO_concentration(); //in ppm. TO BE MODIFIED.
-//   int dust_concentration = (int)sharp_dust_sensor.get_concentration(); //in micro gram per cubic meter.
-//   float lat = gps_sensor.get_lat();
-//   float lon = gps_sensor.get_lon();
-//   //unsigned int lux = lux_sensor.get_lux();
-//   unsigned int lux = 0;
-//   int soil_humidity = soil_moisture.get_moisture();
-//   int soil_temperature = (int)DFR0198.get_temperature();
-//   int air_humidity = dht11.get_humidity();
-//   int air_temperature = dht11.get_temperature();
-  
-//   //Debugging
-//   // Serial.print("O3 concentration is : ");Serial.print(O3_concentration);Serial.println(" ppb");
-//   // delay(1000);
-//   // Serial.print("CO concentration is : ");Serial.print(CO_concentration);Serial.println(" ppm");
-//   // delay(1000);
-//   // Serial.print("Dust concentration is : ");Serial.print(dust_concentration);Serial.println(" ug/m^3");
-//   // delay(1000);
-//   // Serial.print("GPS Coordinates are -> LAT : ");Serial.print(lat,6);Serial.print("  LON : ");Serial.println(lon,6);
-//   // delay(1000);
-//   // Serial.print("Luminosity is : ");Serial.print(lux);Serial.println(" LUX");
-//   // delay(1000);
-//   // Serial.print("Soil humidity is : ");Serial.print(soil_humidity);Serial.println(" %RH");
-//   // delay(1000);
-//   // Serial.print("Soil temperature is : ");Serial.print(soil_temperature,1);Serial.println(" Degrees");
-//   // delay(1000);
-//   // Serial.print("Air humidity is : ");Serial.print(air_humidity);Serial.println(" %RH");
-//   // delay(1000);
-//   // Serial.print("Air temperature is : ");Serial.print(air_temperature);Serial.println(" Degrees");
-//   // delay(1000);
-
-//   //LoRA Communication
-//   // A -> O3 concentration
-//   // B -> Dust concentration
-//   // C -> GPS
-//   // D -> Lux
-//   // E -> Soil humidity
-//   // F -> Soil temperature
-//   // G -> Air humidity
-//   // H -> Air temperature
-
-//   // Trame
-//   // "\\!node1:A=%d:B=%d:C=%d:D=%d:E=%d:F=%d:G=%d:H=%d"
-
-//   //Required for Float representation since %f in sprintf is not supported in Arduino.
-//   //Example
-//   // static float f_val = 123.6794;
-//   // static char outstr[15];
-//   // dtostrf(f_val,7, 3, outstr);
-//   // The output of this is 123.679
-
-//   //lat to string. Lat can be -90.000000 to 90.000000
-//   char lat_string[15];
-//   dtostrf(lat,10, 6, lat_string);
-
-//   //lon to string. Lon can be -180.000000 to 180.000000
-//   char lon_string[15];
-//   dtostrf(lon,11, 6, lon_string);
-
-//   //Lux no need, its an unsigned int.
-
-//   //soil_humidity no need, its an int.
-
-//   //Soil temperature to string.
-//   //char soil_temperature_string[15];
-//   //dtostrf(soil_temperature,5, 1, soil_temperature_string);
-
-//   //sprintf(string, "\\!node1:A=%s:B=%s:C=%s,%s:D=%d:E=%d:F=%s", O3_concentration_string,dust_concentration_string, lat_string, lon_string, lux, soil_humidity,soil_temperature_string);
-//   //sprintf(string, "\\!node1:A=%s:B=%s", O3_concentration_string,dust_concentration_string);
-//   //sprintf(string, "\\!node1:A=%s:B=%s",O3_concentration_string,dust_concentration_string);
-//   //sprintf(string, "\\!node1:A=%s",O3_concentration_string);
-//   //sprintf(string, "\\!node1:A=%s",lat_string);
-
-//   char string[75];
-//   // sprintf(string, "\\!node1:temperature_air=%d:humidity_air=%d", air_temperature,air_humidity);
- 
-//   //The one to send for all sensors
-//   sprintf(string, "\\!node1:A=%d:B=%d:C=%s,%s:D=%d:E=%d:F=%d:G=%d:H=%d", 
-//                     O3_concentration,dust_concentration, lat_string, lon_string, 
-//                      lux, soil_humidity, soil_temperature, air_humidity, air_temperature);
-
-//   Lora_send_string(string);
-
-//   //Serial.print("string is : ");Serial.println(string);
-
-//   delay(1000);
-//   //Serial.println("Die now");//Serial.println(millis(),6);
-//   //delay(1000);
-//   //Serial.println(" ");
-// }
-
-
